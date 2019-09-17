8ee9ac4fa86f80b577671668622dcae8cedeafb7
sudoxx2 <moungpeter@gmail.com>
2019-09-16 17:34:06 -0700

    finished skills section and started projects

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ $(window).on('resize', function () {
   
   $('#size').text($(window).width())
 }).trigger('resize')
+
+let modal = document.getElementById("id01");
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
